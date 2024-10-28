af7e043d8c5087cc75b8b0e4980196167677a6bc
Annes Bani Al Doumi <annesdoumi2@gmail.com>
2024-10-28 16:33:07 +0100

    Config

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,15 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig(({ mode }) => {
-  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
-
-  return {
-    plugins: [vue()],
-    server: {
-      proxy: {
-        '/api': {
-          target: process.env.VITE_API_BASE_URL || 'http://localhost:5000',
-          changeOrigin: true,
-          secure: false,
-        },
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_BASE_URL || 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
       },
     },
-  };
+  },
 });
