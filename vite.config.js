496e64a246df94b0f861ca23c0540793caa42b11
Annes Bani Al Doumi <annesdoumi2@gmail.com>
2024-11-09 23:19:12 +0100

    Neuer Test

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,12 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  base: '/',
   plugins: [vue()],
   server: {
     historyApiFallback: true,
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_BASE_URL || 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
   },
+  build: {
+    outDir: 'dist',
+  },
+  base: './', // Dies hilft, relative Pfade zu verwenden
 });
-
