3e71be0bef0024143b38d96914cb21160db99933
Annes Bani Al Doumi <annesdoumi2@gmail.com>
2024-11-09 23:32:36 +0100

    FINALE

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,15 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
+  base: './', // Relativer Pfad für Assets
   server: {
     historyApiFallback: true,
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_BASE_URL || 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
-  build: {
-    outDir: 'dist',
-  },
-  base: './', // Dies hilft, relative Pfade zu verwenden
 });
