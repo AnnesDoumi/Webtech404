81215685748d5a03bc00b835bffddc8724cb363e
Annes Bani Al Doumi <annesdoumi2@gmail.com>
2025-01-30 21:17:07 +0100

    FINALLY

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,22 +1,44 @@
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
     event.waitUntil(
-        caches.open('app-cache').then((cache) => {
+        caches.open("app-cache").then((cache) => {
             return cache.addAll([
-                '/',
-                '/index.html',
-                '/manifest.json',
-                '/styles.css',
-                '/offline.html',
-                '/icons/icon-192x192.png',
-                '/icons/icon-512x512.png'
+                "/",
+                "/index.html",
+                "/manifest.json",
+                "/offline.html",
+                "/favicon.ico",
+                "/icons/icon-144x144.png",
+                "/icons/icon-192x192.png",
+                "/icons/icon-512x512.png"
             ]);
         })
     );
+    self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        fetch(event.request).catch(() => caches.match('/offline.html'))
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames
+                    .filter((cacheName) => cacheName !== "app-cache")
+                    .map((cacheName) => caches.delete(cacheName))
+            );
+        })
     );
-});;
+    self.clients.claim();
+});
 
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request).then((cachedResponse) => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                } else if (event.request.mode === "navigate") {
+                    return caches.match("/offline.html");
+                }
+            });
+        })
+    );
+});
